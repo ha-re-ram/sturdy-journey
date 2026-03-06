51201655dabe8ca42aa59c5d2da79b23e3d7919c
Hareram Kushwaha <hareramkushwaha054@gmail.com>
2026-03-07 02:09:18 +0530

    fix: explicit static generation for sitemap and robots routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { siteConfig } from "@/lib/config";
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const routes = ["", "/about", "/projects", "/blog"].map((route) => ({
         url: `${siteConfig.url}${route}`,
